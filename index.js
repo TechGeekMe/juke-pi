12cefd4572fbaf167787ac5407056a54da1acbac
shoaib30 <shoaib30@gmail.com>
2016-07-14 22:28:40 +0530

    added upload feature

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var express = require('express')
+
+
+var multer = require('multer')
 var app = express();
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + '.mp3')
+  }
+})
+
+var upload = multer({storage: storage})
 
 var mpd = require('mpd'),
     cmd = mpd.cmd
@@ -7,6 +20,8 @@ var client = mpd.connect({
   port: 6600,
   host: 'localhost',
 });
+
+
 client.on('ready', function() {
   console.log("ready");
 });
@@ -34,6 +49,27 @@ app.get('/pause', function(req, res) {
     console.log(msg);
     res.send('paused');
   });
+})
+app.post('/upload-file',upload.single('myFile'), function(req, res)  {
+  console.log("file: "+req.file.path)
+  console.log("name: "+req.file.originalname)
+  var filePath = 'uploads/' + req.file.originalname + '.mp3';
+  var updateCommand = cmd("update", [filePath]);
+  var addCommand = cmd("add", [filePath])
+  client.sendCommand(updateCommand, function(err, msg) {
+    if (err) throw err;
+    client.sendCommand(addCommand, function(err, msg) {
+      if (err) throw err;
+        res.end();
+    })
+  })
+  // client.sendCommand(cmd("update", [filePath]), function(err, msg) {
+  //   if (err) throw err;
+  //   client.sendCommand(cmd("add", [filePath]), function(err, msg) {
+  //     if (err) throw err;
+  //     res.end();
+  //   })
+  // })
 })
 
 app.use(express.static('public'));
